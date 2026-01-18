d29b17534b4464e5b29213adcffdecb1f30c355b
orosmatthew <orosmatthew@pm.me>
2026-01-18 18:09:18 -0500

    Use consistent duration for ChronoTimePointSerializer

--- include/sbs/serializers/chrono.hpp
+++ include/sbs/serializers/chrono.hpp
@@ -3,6 +3,7 @@
 #include <sbs/sbs.hpp>
 
 #include <chrono>
+#include <cstdint>
 
 namespace sbs {
 
@@ -22,20 +23,24 @@ struct ChronoDurationSerializer {
     }
 };
 
-template <class Clock, class Duration = Clock::duration, class DurationSerializer = DefaultSerializer<Duration>>
+template <
+    class Clock,
+    class Duration = std::chrono::duration<int64_t, std::nano>,
+    class DurationSerializer = DefaultSerializer<Duration>>
     requires(
         sbs::Serializer<DurationSerializer, Duration> && std::copyable<Duration>
         && std::is_default_constructible_v<Duration>)
 struct ChronoTimePointSerializer {
-    void operator()(Archive& ar, std::chrono::time_point<Clock, Duration>& time_point) const
+    void operator()(Archive& ar, std::chrono::time_point<Clock>& time_point) const
     {
         if (ar.serializing()) {
-            const Duration ticks = time_point.time_since_epoch();
-            ar.archive_copy<DurationSerializer>(ticks);
+            const Duration duration = std::chrono::duration_cast<Duration>(time_point.time_since_epoch());
+            ar.archive_copy<DurationSerializer>(duration);
         } else {
             auto duration = Duration();
             ar.archive<DurationSerializer>(duration);
-            time_point = std::chrono::time_point<Clock, Duration> { duration };
+            time_point
+                = std::chrono::time_point<Clock> { std::chrono::duration_cast<typename Clock::duration>(duration) };
         }
     }
 };
@@ -47,11 +52,10 @@ void serialize(Archive& ar, std::chrono::duration<Tick, Period>& duration)
     ChronoDurationSerializer<Tick, TickSerializer, Period>()(ar, duration);
 }
 
-template <class Clock, class Duration = Clock::duration, class DurationSerializer = DefaultSerializer<Duration>>
-    requires(sbs::Serializer<DurationSerializer, Duration>)
-void serialize(Archive& ar, std::chrono::time_point<Clock, Duration>& time_point)
+template <class Clock>
+void serialize(Archive& ar, std::chrono::time_point<Clock>& time_point)
 {
-    ChronoTimePointSerializer<Clock, Duration, DurationSerializer>()(ar, time_point);
+    ChronoTimePointSerializer<Clock>()(ar, time_point);
 }
 
 }
